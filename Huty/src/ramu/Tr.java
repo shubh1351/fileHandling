e7f694b2875a9da84965014a06906cac4d7f819a
shubh <shubh@Shubh_51>
2024-07-07 12:25:22 +0530

    dhdhjsj

---
+++ Huty/src/ramu/Tr.java
@@ -0,0 +1,9 @@
+package ramu;
+
+public class Tr {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
---
+++ Huty/src/ramu/We.java
@@ -0,0 +1,10 @@
+package ramu;
+
+public class We {
+
+	public static void main(String[] args) {
+		System.out.println("you");
+
+	}
+
+}
